b90f31921bb1c43d678460eb4db1459b861a59e7
Ert15 <157598116+Ert15@users.noreply.github.com>
2024-02-18 20:51:52 +0600

    Add files via upload

---
+++ tc4.3.py
@@ -0,0 +1,43 @@
+import math
+
+d= float(input())
+r= math.radians(d)
+print(r)
+print("end")
+
+
+
+h = float(input())
+b1 = float(input())
+b2 = float(input())
+
+a = 0.5 * (b1 + b2) * h
+print(a)
+print("end")
+
+
+
+
+
+
+
+n = int(input())
+s = float(input())
+
+a = (n * s**2) / (4 * math.tan(math.pi / n))
+print(a)
+print("end")
+
+
+
+
+
+b = float(input())
+h = float(input())
+
+a = b * h
+print(a)
+print("end")
+
+
+
