ab15632edd11ed7abb09bd08d34f07ef32ea7476
Ert15 <157598116+Ert15@users.noreply.github.com>
2024-02-18 20:51:05 +0600

    Add files via upload

---
+++ tc4.1.py
@@ -0,0 +1,40 @@
+import datetime
+
+x = datetime.datetime.now()
+
+b = int( x.strftime("%d"))
+
+print(b-5)
+print("end")
+
+
+
+
+
+x = datetime.datetime.now()
+
+b = int( x.strftime("%d"))
+
+print(b-1)
+print(b)
+print(b+1)
+print("end")
+
+
+
+
+x = datetime.datetime.now()
+
+print(x.strftime("%f"))
+print(x.strftime("%x"))
+print(x.strftime("%X"))
+print("end")
+
+
+
+
+
+
+
+
+
